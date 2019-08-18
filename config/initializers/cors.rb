f6ef6257a504ebf79d294d762c9adb17c0be8980
Victor Zhang <victorzw895@gmail.com>
2019-08-18 11:40:48 +1000

    syntax change in cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
         origins '*'
         resource '*',
-            :headers => :any,
-            :methods => %i(get post put patch delete options head) #array of symbols
+            # :headers => :any,
+            headers: :any,
+            # :methods => %i(get post put patch delete options head) #array of symbols
+            methods: [:get, :post, :put, :patch, :delete, :options, :head] #array of symbols
     end
 end
