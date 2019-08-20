4085a63d583a470385f13d4d38849d9a1a4186c4
Victor Zhang <victorzw895@gmail.com>
2019-08-20 21:49:44 +1000

    fix merge schema

--- db/schema.rb
+++ db/schema.rb
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2019_08_20_105805) do
 
   create_table "tags", force: :cascade do |t|
     t.text "tag_name"
+    t.integer "dripple_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
